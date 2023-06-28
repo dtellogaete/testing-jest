296b886dfc1f763769612d28affe80383c0e9d3f
= <=>
2023-06-27 21:38:48 -0600

    update test & README

---
+++ README.md
@@ -0,0 +1,26 @@
+# Pruebas con Jest y Supertest para la API de Cafés
+
+Este repositorio contiene pruebas automatizadas utilizando Jest y Supertest para verificar el comportamiento de la API de Cafés.
+
+## Ejecución de las pruebas
+
+Para ejecutar las pruebas, utiliza el siguiente comando:
+
+```bash
+npm run test
+```
+
+Esto ejecutará los casos de prueba definidos en el archivo `server.spec.js` utilizando Jest.
+
+## Descripción de las pruebas
+
+### Endpoint de Cafés
+
+- `Status 200`: Verifica que se obtenga un código de estado 200 al realizar una solicitud GET a la ruta `/cafes`. También comprueba que el tipo de dato de la respuesta sea un objeto.
+
+- `Status 404 al eliminar café que no existe`: Comprueba que se obtenga un código de estado 404 al intentar eliminar un café con un ID que no existe. La solicitud DELETE se realiza a la ruta `/cafes/:id`, donde `:id` es un ID de café inexistente. Se establece un token de autorización ficticio.
+
+- `Agrega un nuevo café`: Verifica que se obtenga un código de estado 201 al agregar un nuevo café. La solicitud POST se realiza a la ruta `/cafes` y se envía un objeto JSON con la información del café, incluyendo un ID único y el nombre del café.
+
+- `Actualiza un café con id diferente`: Comprueba que se obtenga un código de estado 400 al intentar actualizar un café con un ID diferente alos existentes. La solicitud PUT se realiza a la ruta `/cafes/:id`, donde `:id` es el ID del café que se desea actualizar. Se envía un objeto JSON con la información del café, incluyendo un ID diferente al del café que se desea actualizar.
+
--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,7 +2,6 @@ const request = require("supertest");
 const server = require("../index");
 
 describe("Endpoint de Cafes", () => {
-
     it("Status 200", async () => {
         const response = await request(server).get("/cafes").send();
         const status = response.statusCode
@@ -14,13 +13,39 @@ describe("Endpoint de Cafes", () => {
         const type = typeof(response.body);
         expect(type).toEqual("object");
     });
-
 });
 
-describe("Status 404 al eliminar café que no existe", () => {    
-    it("Status 404", async () => {
-        const response = await request(server).delete("/cafes/20").send();
-        const status = response.statusCode
-        expect(status).toEqual(404);
+describe("Status 404 al eliminar café que no existe", () => {
+    it("Código 404", async () => {
+        const response = await request(server)
+            .delete("/cafes/20")
+            .set("Authorization", "token"); 
+
+        expect(response.status).toBe(404);
+    });
+});
+
+describe("Agrega un nuevo café", () => {
+    it("201 al agregar un nuevo café", async () => {
+        const response = await request(server)
+            .post("/cafes")
+            .send({
+                id: 10,
+                nombre: "Café de Olla",                
+            });
+
+        expect(response.status).toBe(201);
+    });
+});
+
+describe("Actualiza un café con id diferente", () => {
+    it("400 el id es diferente", async () => {
+        const response = await request(server)
+            .put("/cafes/20")
+            .send({
+                id: 10,
+                nombre: "Café de Olla",                
+            });
+        expect(response.status).toBe(400);
     });
 });
